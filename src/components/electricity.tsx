bcf3acbd45ed5763e1e9c75023f835cfe7071d8d
Daria <dariastoicad@gmail.com>
2023-12-20 22:48:21 +0100

    electricity chart math

--- src/components/electricity.tsx
+++ src/components/electricity.tsx
@@ -1,3 +1,4 @@
+import { getHourFromDate } from "@/utils/time";
 import Widget from "./widget";
 
 export default async function Electricity() {
@@ -56,11 +57,10 @@ export default async function Electricity() {
 
   const todayPrice = data.viewer.homes[0].currentSubscription.priceInfo.today;
 
-  todayPrice[2].total = -0.6;
-  todayPrice[3].total = -0.4;
-
   const maxPrice = Math.max(...todayPrice.map((price) => price.total));
   const minPrice = Math.min(...todayPrice.map((price) => price.total), 0);
+  const firstQuartile = (maxPrice + minPrice) * 0.33;
+  const secondQuartile = (maxPrice + minPrice) * 0.66;
 
   function normalize(min: number, max: number, value: number) {
     return (value - min) / (max - min);
@@ -79,33 +79,65 @@ export default async function Electricity() {
     return "green";
   }
 
+  function convertToOre(num: number) {
+    return Math.round(num * 100);
+  }
+
+  function chartFadeColor(hour) {
+    let currentHour = parseInt(getHourFromDate(new Date()));
+    if (hour <= currentHour) {
+      return {
+        opacity: "60%",
+      };
+    }
+  }
+
   return (
     <Widget
       size="medium"
-      className="bg-gradient-to-tr from-[#BCBBFF] to-[#DAD9FF] flex items-center justify-center px-2 py-4"
+      className="bg-gradient-to-tr from-[#BCBBFF] to-[#DAD9FF] flex justify-between items-center px-2"
     >
-      {todayPrice.map((item, index) => (
-        <div
-          key={index}
-          className="flex flex-col items-center justify-end h-full"
-        >
-          <div
-            className="w-4"
-            style={{
-              height: normalize(minPrice, maxPrice, item.total) * 100 + "%",
-              backgroundColor: getPriceColor(item.total),
-            }}
-          ></div>
-
-          {index % 2 === 0 ? (
-            <p className="text-center text-xs">
-              {String(index).padStart(2, "0")}
-            </p>
-          ) : (
-            <p className="text-center text-xs invisible">00</p>
-          )}
+      <div className="flex flex-col justify-between h-full pt-2 pb-7">
+        <p className="text-xs">{convertToOre(maxPrice)}</p>
+        <p className="text-xs">{convertToOre(secondQuartile)}</p>
+        <p className="text-xs">{convertToOre(firstQuartile)}</p>
+        <p className="text-xs">{convertToOre(minPrice)}</p>
+      </div>
+      <div className="flex flex-col h-full py-4 gap-2">
+        <div className="flex items-center h-full justify-center relative">
+          <div className="h-[1px] w-full bg-black/20 absolute top-0"></div>
+          <div className="h-[1px] w-full bg-black/20 absolute top-[33%]"></div>
+          <div className="h-[1px] w-full bg-black/20 absolute top-[66%]"></div>
+          {todayPrice.map((item, index) => (
+            <div
+              key={index}
+              className="flex flex-col items-center justify-end h-full"
+            >
+              <div
+                className="w-4"
+                style={{
+                  height: normalize(minPrice, maxPrice, item.total) * 100 + "%",
+                  backgroundColor: getPriceColor(item.total),
+                  ...chartFadeColor(index),
+                }}
+              ></div>
+            </div>
+          ))}
         </div>
-      ))}
+        <div className="flex justify-between">
+          {todayPrice.map((item, index) => (
+            <div key={index} className="flex flex-col">
+              {index % 2 === 0 ? (
+                <p className="text-center text-xs leading-none">
+                  {String(index).padStart(2, "0")}
+                </p>
+              ) : (
+                <p className="text-center text-xs invisible leading-none">00</p>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
     </Widget>
   );
 }
